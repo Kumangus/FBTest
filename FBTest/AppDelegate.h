90b6e9c541abcb0372a0bd62be396f715af81872
mac <mac@macteki-MacBook-5.local>
2013-11-04 11:04:16 +0800

    Initial Commit

---
+++ FBTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FBTest
+//
+//  Created by mac on 13/11/4.
+//  Copyright (c) 2013年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
